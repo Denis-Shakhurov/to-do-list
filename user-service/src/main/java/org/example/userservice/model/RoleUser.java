90593ef2d40d273bac458762e9438d38ae6a9aae
DenisShakhurov <disa.sdf86@gmail.com>
2025-08-17 07:58:59 +0700

    #FIX RoleUser for create user

--- user-service/src/main/java/org/example/userservice/model/RoleUser.java
+++ user-service/src/main/java/org/example/userservice/model/RoleUser.java
@@ -1,6 +1,30 @@
 package org.example.userservice.model;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum RoleUser {
-    USER,
-    ADMIN
+    USER("user"),
+    ADMIN("admin");
+
+    private final String value;
+
+    RoleUser(String value) {
+        this.value = value;
+    }
+
+    @JsonCreator
+    public static RoleUser fromString(String value) throws IllegalAccessException {
+        for (RoleUser role : RoleUser.values()) {
+            if (role.value.equalsIgnoreCase(value)) {
+                return role;
+            }
+        }
+        throw new IllegalAccessException("Unknown role: " + value);
+    }
+
+    @JsonValue
+    public String getValue() {
+        return value;
+    }
 }
