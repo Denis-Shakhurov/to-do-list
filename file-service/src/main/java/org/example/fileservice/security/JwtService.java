659061aa3529c8ad12418b5976693f2e06f530f4
DenisShakhurov <disa.sdf86@gmail.com>
2025-08-24 08:20:43 +0700

    #ADD validate token for file-service

---
+++ file-service/src/main/java/org/example/fileservice/security/JwtService.java
@@ -0,0 +1,46 @@
+package org.example.fileservice.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+
+@Service
+public class JwtService {
+    @Value("${jwt.secret}")
+    private String secret;
+
+    public boolean validationToken(String token, UserDetails userDetails) {
+        try {
+            String userName = extractUserName(token);
+            return userName.equals(userDetails.getUsername()) && !isExpiredToken(token);
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public String extractUserName(String token) {
+        return extractClaims(token).getSubject();
+    }
+
+    public boolean isExpiredToken(String token) {
+        return extractClaims(token).getExpiration().before(new Date());
+    }
+
+    private Claims extractClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(getKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    private SecretKey getKey() {
+        return Keys.hmacShaKeyFor(secret.getBytes());
+    }
+}
